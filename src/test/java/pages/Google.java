57ba3a592f0c1931338e1fde60724613fd4734f6
Maksym <77988267+MaksymYefimenko@users.noreply.github.com>
2021-03-18 19:07:58 +0100

    Code optimization

--- src/test/java/pages/Google.java
+++ src/test/java/pages/Google.java
@@ -1,43 +1,41 @@
 package pages;
 
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.*;
 import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import javax.swing.*;
+import javax.swing.event.InternalFrameListener;
+import java.awt.*;
+
+import static com.codeborne.selenide.Selenide.*;
 
 
-public class Google{
+public class Google {
 
-    SelenideElement battunIGre=$("#introAgreeButton");
-    SelenideElement search=$(By.name("q"));
+    SelenideElement battunIGre = $("#introAgreeButton");
+    SelenideElement search = $(By.name("q"));
 
 
     @Before
-    public void setUp(){
-        Configuration.browser="chrome";
+    public void setUp() {
+        Configuration.browser = "chrome";
 
     }
 
     @Test
-    public void gotoGoogle(){
+    public void gotoGoogle() {
         open("http://google.com");
-        Selenide.switchTo().frame($("iframe"));
-        battunIGre.shouldBe(Condition.visible).click();
+        if (true) {
+            switchTo().frame($("iframe"));
+            battunIGre.shouldBe(Condition.visible).click();
+        } else {
+            search.sendKeys("Selenide");
+            search.sendKeys(Keys.ENTER);
+        }
     }
-
-    @Test
-    public void serchForm(){
-        search.sendKeys("Selenide");
-        search.sendKeys(Keys.ENTER);
-
-    }
-
 }
+
